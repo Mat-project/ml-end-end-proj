d9553a3b3f6564d3c307dc0828eb43d698d6a85b
Mat-project <mathan21092006@gmail.com>
2025-01-18 17:55:37 +0530

    Data validation 2 succesfully

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from mlProject.Logging import logger
 from mlProject.pipeline.stage_01_data_ingesion import DataIngesionTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 stage_name="Data Ingesion stage"
 try:
     logger.info(f">>>>>stage {stage_name} startrd <<<<<")
     obj=DataIngesionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {stage_name} compleated <<<<<\n\nx=========")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+stage_name="Data Validation stage"
+try:
+    logger.info(f">>>>>stage {stage_name} startrd <<<<<")
+    obj=DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>> stage {stage_name} compleated <<<<<\n\nx=========")
 except Exception as e:
